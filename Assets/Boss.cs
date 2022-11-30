fb5870a2ea82398366ca90017789222999b5f876
Levi Horst <levi.horst@bruins.belmont.edu>
2022-11-30 13:28:17 -0600

    Worked on boss script and animations

---
+++ Assets/Boss.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+
+    public Transform player;
+    public bool isFlipped = false;
+    public float offset = 0;
+
+    Rigidbody2D rb;
+
+    public void LookAtPlayer()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Vector3 flipped = transform.localScale;
+        flipped.z *= -1f;
+
+        if (transform.position.x > player.position.x + offset && isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+
+            Vector2 newPos = new Vector2(offset, 0f);
+            rb.MovePosition(newPos);
+
+            isFlipped = false;
+            offset *= -1;
+        }
+        else if (transform.position.x < player.position.x + offset && !isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+
+            Vector2 newPos = new Vector2(offset, 0f);
+            rb.MovePosition(newPos);
+
+            isFlipped = true;
+            offset *= -1;
+        }
+    }
+}
