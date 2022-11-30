03a69c2e0a46c0b4b16ffcfc60ac858839005b2f
Levi Horst <levi.horst@bruins.belmont.edu>
2022-11-30 16:13:02 -0600

    Fixed boss turnaround issue

--- Assets/Boss.cs
+++ Assets/Boss.cs
@@ -8,6 +8,7 @@ public class Boss : MonoBehaviour
     public Transform player;
     public bool isFlipped = false;
     public float offset = 0;
+    public float secoffset = 1;
 
     Rigidbody2D rb;
 
@@ -22,22 +23,24 @@ public class Boss : MonoBehaviour
             transform.localScale = flipped;
             transform.Rotate(0f, 180f, 0f);
 
-            Vector2 newPos = new Vector2(offset, 0f);
-            rb.MovePosition(newPos);
+            Vector2 newPos = new Vector2(rb.position.x - offset - secoffset, rb.position.y);
+            transform.position = newPos;
 
             isFlipped = false;
             offset *= -1;
+            secoffset *= -1;    
         }
         else if (transform.position.x < player.position.x + offset && !isFlipped)
         {
             transform.localScale = flipped;
             transform.Rotate(0f, 180f, 0f);
 
-            Vector2 newPos = new Vector2(offset, 0f);
-            rb.MovePosition(newPos);
+            Vector2 newPos = new Vector2(rb.position.x - offset - secoffset, rb.position.y);
+            transform.position = newPos;
 
             isFlipped = true;
             offset *= -1;
+            secoffset *= -1;
         }
     }
 }
